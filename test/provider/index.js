29e208f406e4388400dcf31d4af728d9a50c58e9
Tarcio <tarcio@gmail.com>
2016-06-29 18:07:52 +1000

    Improving Mocha interface and updating with latest Pact JS API

--- test/provider/index.js
+++ test/provider/index.js
@@ -7,19 +7,7 @@ server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
 
 server.get('/projects', function (req, res) {
-  res.json([
-    {
-      id: 1,
-      name: 'Project 1',
-      due: '2016-02-11T09:46:56.023Z',
-      tasks: [
-        {id: 1, name: 'Do the laundry', 'done': true},
-        {id: 2, name: 'Do the dishes', 'done': false},
-        {id: 3, name: 'Do the backyard', 'done': false},
-        {id: 4, name: 'Do nothing', 'done': false}
-      ]
-    }
-  ])
+  res.json({ reply: 'hello' })
 })
 
 server.get('/providerStates', function (req, res) {
